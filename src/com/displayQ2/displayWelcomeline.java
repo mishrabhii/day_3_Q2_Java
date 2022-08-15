d2dd856562a45508ae450384f7b58aa9aaa5c17a
mishrabhii <abhi.mishra509@gmail.com>
2022-08-15 14:07:28 +0530

    initil commit

---
+++ src/com/displayQ2/displayWelcomeline.java
@@ -0,0 +1,9 @@
+package com.displayQ2;
+
+public class displayWelcomeline {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Start with Displaying Welcome to Line \r\n  Comparison Computation Program on \r\n           Master Branch ");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module displayQ2 {
+}
