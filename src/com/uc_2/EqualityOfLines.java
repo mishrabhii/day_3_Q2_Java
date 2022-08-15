9298c4cdcf8e70b3c3a5946f8843714768426efe
mishrabhii <abhi.mishra509@gmail.com>
2022-08-16 00:49:28 +0530

    check equality of two lines

---
+++ src/com/uc_2/EqualityOfLines.java
@@ -0,0 +1,16 @@
+package com.uc_2;
+
+public class EqualityOfLines {
+	
+	public static void main(String[] args) {
+		int lengthLine1 = 2;
+		int lengthLine2 = 2;		
+		int d1 = (int) (lengthLine1);
+		int d2 = (int)(lengthLine2);
+		if (d1==(d2))
+            System.out.println("Line 1 and line 2 have Equal lengths.");
+        else
+            System.out.println("Line 1 and Line 2 do not have Equal lengths.");
+	}
+}
+
